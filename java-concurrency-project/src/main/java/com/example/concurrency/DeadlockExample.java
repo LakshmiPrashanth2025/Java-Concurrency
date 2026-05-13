af2719ad50ee1db00b2e8291d07bbd528c65d735
LakshmiPrashanth2025 <lakshmi.prashant@gmail.com>
2026-05-13 14:07:46 +0530

    Add DeadlockExample class to demonstrate deadlock

---
+++ java-concurrency-project/src/main/java/com/example/concurrency/DeadlockExample.java
@@ -0,0 +1,31 @@
+public class DeadlockExample {
+
+    static final Object lock1 = new Object();
+    static final Object lock2 = new Object();
+
+    public static void main(String[] args) {
+
+        Thread t1 = new Thread(() -> {
+
+            synchronized (lock1) {
+
+                synchronized (lock2) {
+                    System.out.println("Thread1");
+                }
+            }
+        });
+
+        Thread t2 = new Thread(() -> {
+
+            synchronized (lock2) {
+
+                synchronized (lock1) {
+                    System.out.println("Thread2");
+                }
+            }
+        });
+
+        t1.start();
+        t2.start();
+    }
+}
